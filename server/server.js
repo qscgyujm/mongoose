255d51169626fe166fb6e4be3cdc0ca1b69cbcd9
Frog Hung (GIS) <qscgyujm@gmail.com>
2018-06-28 15:52:57 +0800

    Add Book API Route

--- server/server.js
+++ server/server.js
@@ -7,10 +7,17 @@ const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const Book = require('./models/Book');
 
+// 建立express
 const app = express();
+// 讀取req 所需套件
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+// bodyParser.json()可以讓 express 讀取json
+app.use(bodyParser.urlencoded({ extended: false }));
+// 
+
+const port = 3000;
 
 // 新增 到 mongo DB
 app.post('/todos', (req, res) => {
@@ -58,7 +65,7 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-
+// delete 
 app.delete('/todos/:id', (req, res) => {
   // get the ID
   const id = req.params.id;
@@ -75,6 +82,7 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
+// 修改
 app.patch('/todos/:id', (req, res) => {
   const id = req.params.id;
   const body = _.pick(req.body, ['text', 'completed']);
@@ -105,15 +113,80 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 
-// app.post('/test', (req, res) => {
-//   console.log(req.body);
-//   res.send(req.body);
-// })
+// Book API
+// Get All
+app.get('/book', (req, res) => {
+  Book.find({}).then(books => {
+    // console.log(books);
+    res.send(books);
+  })
+})
+
+// Get one by Id
+app.get('/book/:id', (req, res) => {
+  Book.findById(req.params.id)
+    .then(book => {
+      res.json(book);
+    });
+});
+
+//  POST Book insert
+app.post('/book', (req, res) => {
+  const newBook = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    category: req.body.category
+  });
+
+  newBook.save().then(book => {
+    if (!book) {
+      res.status(404).send();
+    }
+    res.json(book);
+  });
+});
+
+// 新增第2種方法
+app.post('/book2', (req, res) => {
+  Book.create(req.body).then(book => {
+    if (!book) return res.status(404).send();
+
+    res.json(book);
+  })
+});
+
+// 修改
+app.put('/book/:id', (req, res) => {
+  Book.findByIdAndUpdate(req.params.id, {
+    $set: {
+      title: req.body.title
+    }
+  }, {
+      new: true
+    })
+    .then(book => {
+      if (!book) {
+        res.status(404).send();
+      }
+      res.json(book);
+    });
+})
+
+app.delete('/book/:id', (req, res) => {
+  Book.findByIdAndRemove(req.params.id)
+    .then(book => {
+      if (!book) return res.status(404).send();
+      res.json(book);
+    }).then(err => {
+      res.send(err);
+    });
+
+})
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
 
-  console.log('started on port 3000');
+  console.log('started on port=', port);
 })
 
 module.exports = { app };
